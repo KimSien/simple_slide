ada42d8a24d8e32a254a91122527a45a0f73961a
kim <tksok2009@gmail.com>
2019-01-14 17:53:01 +0900

    add config setting

--- asset/js/slide.js
+++ asset/js/slide.js
@@ -1,8 +1,16 @@
-
+/**
+ * 
+ *  jQuery Simple Slide
+ * 
+ * 
+ * 
+ */
 var _slide = _slide || {
-    _setting : '',
-    run : {},
-    s_navi_current :{},
+    x_position: '', current_slide: 0, first: 0,
+    s_width: '', s_length: '', numbers: 0, tempo: 0,
+    run: {},
+    set_animation: {},
+    s_navi_current: {},
     s_message: {},
     put_message: {},
     s_navi: {}
@@ -10,119 +18,126 @@ var _slide = _slide || {
 
 var slide = _slide;
 
-var x_position = 0, current_slide = 0, first = 0;
-var s_width, s_length,numbers=0;
+_slide.run = function (_array) {
 
-_slide.run = function(){
+    Object.keys(_array).forEach(function (key) {
+        console.log(key, _array[key]);
+        _slide[key] = _array[key];
 
+    });
 
     $(window).on('load resize', function () {
 
-        s_width = $("#slide-contains").width();
+        _slide.s_width = $("#slide-contains").width();
 
-        s_length = $("img.slide-data").length;
+        _slide.s_length = $("img.slide-data").length;
 
-        $(".slide-row").width(s_width * s_length);
+        $(".slide-row").width(_slide.s_width * _slide.s_length);
 
-        $("img.slide-data").width(s_width);
+        $("img.slide-data").width(_slide.s_width);
 
-        x_position = -(current_slide * s_width);
+        _slide.x_position = -(_slide.current_slide * _slide.s_width);
 
-        $(".slide-row").animate({ "margin-left": x_position }, 0);
+        $(".slide-row").animate({ "margin-left": _slide.x_position }, 0);
 
-        if (first == 0) {
+        if (_slide.first == 0) {
             // Slide Navi make
-            for (let index = 0; index < s_length; index++) {
+            for (let index = 0; index < _slide.s_length; index++) {
                 _slide.s_navi(index);
             }
             // Slide Message make
             _slide.s_message();
-            _slide.put_message(current_slide);
+            _slide.put_message(_slide.current_slide);
         }
-        first = 1;
+        _slide.first = 1;
 
+        _slide.set_animation();
     });
 
 }
 
 
-    $(window).on('load',function(){
-        $("#s_button .before").on("click", function () {
+$(window).on('load', function () {
+    $("#s_button .before").on("click", function () {
 
-            if (current_slide > 0) {
-                x_position = x_position + s_width;
-                $(".slide-row").animate({ "margin-left": x_position }, 500);
-                current_slide--;
-                _slide.s_navi_current(current_slide);
+        if (_slide.current_slide > 0) {
+            _slide.x_position = _slide.x_position + _slide.s_width;
+            $(".slide-row").animate({ "margin-left": _slide.x_position }, 500);
+            _slide.current_slide--;
+            _slide.s_navi_current(_slide.current_slide);
 
-            }
-
-        })
-
-        $("#s_button .next").on("click", function () {
-
-            if (current_slide < (s_length - 1)) {
-                x_position = x_position - s_width;
-                $(".slide-row").animate({ "margin-left": x_position }, 500);
-                current_slide++;
-                _slide.s_navi_current(current_slide);
-            }
-
-        })
-    })
-
-
-    _slide.s_navi_current = function(numbers) {
-
-        $("#slide-thumbnail li").removeClass("current");
-        $("#navi-" + (numbers)).addClass("current");
-        _slide.put_message(numbers);
-
-    }
-
-    _slide.s_navi = function(numbers) {
-
-        _template = "<li id='navi-" + numbers + "'>" + numbers + "</li>";
-
-        $("#slide-thumbnail").append(_template);
-        $("#navi-" + numbers).on("click", function () {
-
-            x_position = -(s_width * numbers)
-            $(".slide-row").animate({ "margin-left": -(s_width * numbers) }, 500);
-            _slide.s_navi_current(numbers);
-            current_slide = numbers;
-
-        });
-
-        if (numbers == 0) {
-            $("#navi-" + (numbers)).addClass("current");
         }
 
+    })
+
+    $("#s_button .next").on("click", function () {
+
+        if (_slide.current_slide < (_slide.s_length - 1)) {
+            _slide.x_position = _slide.x_position - _slide.s_width;
+            $(".slide-row").animate({ "margin-left": _slide.x_position }, 500);
+            _slide.current_slide++;
+            _slide.s_navi_current(_slide.current_slide);
+        }
+
+    })
+})
+
+
+_slide.s_navi_current = function (num) {
+
+    $("#slide-thumbnail li").removeClass("current");
+    $("#navi-" + (num)).addClass("current");
+    _slide.put_message(num);
+
+}
+
+_slide.s_navi = function (num) {
+
+    _template = "<li id='navi-" + num + "'>" + num + "</li>";
+
+    $("#slide-thumbnail").append(_template);
+    $("#navi-" + num).on("click", function () {
+
+        _slide.x_position = -(_slide.s_width * num)
+        $(".slide-row").animate({ "margin-left": -(_slide.s_width * num) }, 500);
+        _slide.s_navi_current(num);
+        _slide.current_slide = num;
+
+    });
+
+    if (_slide.numbers == 0) {
+        $("#navi-" + (_slide.numbers)).addClass("current");
     }
 
-    _slide.s_message = function(){
-        message_list = $(".slide-message li");
-        console.log(message_list);
+}
+
+_slide.s_message = function () {
+    message_list = $(".slide-message li");
+}
+
+_slide.put_message = function (num) {
+    $("#slide-message-view").html(message_list[num]);
+}
+
+
+// slide animation
+_slide.set_animation = function () {
+
+    if(_slide.tempo > 0){
+        setInterval(function () {
+
+            _slide.numbers++;
+            if (_slide.numbers > 2) { _slide.numbers = 0 }
+
+            _slide.x_position = -(_slide.s_width * _slide.numbers)
+
+            $(".slide-row").animate({ "margin-left": -(_slide.s_width * _slide.numbers) }, 500);
+            _slide.s_navi_current(_slide.numbers);
+
+            _slide.current_slide = _slide.numbers;
+
+        }, _slide.tempo);
     }
-
-    _slide.put_message = function(num){
-        $("#slide-message-view").html(message_list[num]);
-    }
-
-
-    // slide animation
-    setInterval(function(){
-
-        numbers ++ ;        
-        if(numbers > 2){numbers=0}
-        
-        x_position = -(s_width * numbers)
-
-        $(".slide-row").animate({ "margin-left": -(s_width * numbers) }, 500);
-        _slide.s_navi_current(numbers);
-
-        current_slide = numbers;
-    },3000);
-
+}
 
 
